4f6cef19216a2d18e458100ba50a243379513b7c
alexandre pinto <alexandrepinto>
2025-05-03 20:15:26 -0300

    save

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(AppWidget(title: "flutterando"));
+}
+
+class AppWidget extends StatelessWidget{
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+
+@override
+  Widget build(BuildContext context) {
+  return MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.red),
+    home: HomePage(),
+  );
+}
+}
+
+class HomePage extends StatefulWidget{
+@override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: GestureDetector(
+        child: Text("contador $counter"), onTap: () {
+          setState((){
+            counter++;
+    
+          });
+      })),
+    );
+  }
+
+}
